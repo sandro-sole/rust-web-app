0fbb7bb25097fe6ea8fcdc2bdae2ed5e5abf4b30
sandro-dev <68480931+sandro-sole@users.noreply.github.com>
2023-10-17 09:55:56 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,6 @@ use tracing_subscriber::EnvFilter;
 #[tokio::main]
 async fn main() -> Result<()> {
 	tracing_subscriber::fmt()
-		.without_time() // For early local development.
-		.with_target(false)
 		.with_env_filter(EnvFilter::from_default_env())
 		.init();
 
